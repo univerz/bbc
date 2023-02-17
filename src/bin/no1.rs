2311d81f0faa8f997e062d120b491d0f35f73daa
univerz <univerz@fu-solution.com>
2023-02-18 00:22:08 +0100

    wip

--- src/bin/no1.rs
+++ src/bin/no1.rs
@@ -4,21 +4,21 @@ use bbc::ui_dbg;
 use color_eyre::eyre::Result;
 use derivative::Derivative;
 use owo_colors::OwoColorize;
-use std::{fmt, str::FromStr};
+use std::collections::VecDeque;
+use std::{
+    fmt,
+    io::{self, Write},
+    str::FromStr,
+};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode, screen::IntoAlternateScreen};
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 pub enum Err {
     Halt,
     StepLimit,
     Unreachable,
 }
 
-// #[derive(Clone, Copy, Debug, PartialEq)]
-// enum Item {
-//     S(u8),
-//     Exp { block: u8, exp: usize, visited: bool },
-//     Unreachable,
-// }
 #[derive(Derivative)]
 #[derivative(PartialEq)]
 #[derive(Clone, Copy, Debug)]
@@ -94,44 +94,116 @@ impl Configuration {
         }
     }
 
-    fn leap(&mut self) -> bool {
-        // return false;
+    fn leap(&mut self, blocks: RefBlocks) -> bool {
+        let [ltape, rtape] = &mut self.tape;
+        // stops before new block compression `01 a^61652 10 E>  d^11104  a^85609885 10`
+        // if self.head.state != 0 && self.head.direction == Direction::Right {
+        //     if let Some(Item::Exp { block: 3, exp: _, visited: _ }) = rtape.last().copied() {
+        //         return true;
+        //     }
+        // }
 
-        // ? A> (110)^2n -> (011)^2n A>
         if self.head.state == 0 && self.head.direction == Direction::Right {
-            if let Some(&Item::Exp { block: 0, exp: mut item_exp, visited: _ }) = self.tape[1].last() {
-                // we want (110)^n n>=2 to avoid degenerate n.div_mod == 0,1 case
-                if item_exp > 1 {
-                    self.tape[1].pop();
-                    if item_exp % 2 == 1 {
-                        item_exp -= 1;
-                        self.tape[1].push(Item::Exp { block: 0, exp: 1, visited: false });
+            // // right tape endgame  `A> a^a 1010 a^b 11` & `a, b % 2 == 0` -> `<C 10 a^(a-2) 1010 a^(b+4) 11` // --conf "!  A> a^4 1010 a^2 11"
+            // if let [
+            //     Item::S(1),
+            //     Item::S(1),
+            //     Item::Exp { block: 0, exp: exp_b, visited: _ },
+            //     Item::S(0),
+            //     Item::S(1),
+            //     Item::S(0),
+            //     Item::S(1),
+            //     Item::Exp { block: 0, exp: exp_a, visited: _ },
+            // ] = rtape.as_mut_slice()
+            // {
+            //     if *exp_a >= 4 && *exp_a % 2 == 0 && *exp_b % 2 == 0 {
+            //         self.head.state = 2;
+            //         self.head.direction = Direction::Left;
+            //         *exp_a -= 2;
+            //         *exp_b = exp_b.checked_add(4).unwrap();
+            //         rtape.push(Item::S(0));
+            //         rtape.push(Item::S(1));
+            //         return true;
+            //     }
+            // }
+            // // right tape endgame  `A> a^a 1010 a^b 10 ^c 11` & `a, c % 2 == 0, b % 2 == 1` -> `<C 10 a^(a-2) 1010 a^(b+4) 10 a^c 11` // --conf "!  A> a^4 1010 a^1 10 a^2 11"
+            // if let [
+            //     Item::S(1),
+            //     Item::S(1),
+            //     Item::Exp { block: 0, exp: exp_c, visited: _ },
+            //     Item::S(0),
+            //     Item::S(1),
+            //     Item::Exp { block: 0, exp: exp_b, visited: _ },
+            //     Item::S(0),
+            //     Item::S(1),
+            //     Item::S(0),
+            //     Item::S(1),
+            //     Item::Exp { block: 0, exp: exp_a, visited: _ },
+            // ] = rtape.as_mut_slice()
+            // {
+            //     if *exp_a >= 4 && *exp_a % 2 == 0 && *exp_b % 2 == 1 && *exp_c % 2 == 0 {
+            //         self.head.state = 2;
+            //         self.head.direction = Direction::Left;
+            //         *exp_a -= 2;
+            //         *exp_b = exp_b.checked_add(4).unwrap();
+            //         rtape.push(Item::S(0));
+            //         rtape.push(Item::S(1));
+            //         return true;
+            //     }
+            // }
+
+            // A> a^2n -> a^2n A> || A> a^2n+1 -> a^2n+1 B> || A> d^n -> c^n A>
+            if let Some(Item::Exp { block, exp: item_exp, visited: _ }) = rtape.last().copied() {
+                for (from_block, to_block) in [(0, 0), (3, 2)] {
+                    if block == from_block {
+                        if item_exp > 1 {
+                            rtape.pop();
+                            // A -> B if  A> a^2n+1
+                            if from_block == 0 && item_exp % 2 == 1 {
+                                self.head.state = 1
+                            }
+                            match ltape.last_mut() {
+                                Some(Item::Exp { block, exp, visited }) if *block == to_block => {
+                                    *exp = exp.checked_add(item_exp).unwrap();
+                                    *visited = true;
+                                    ui_dbg!("\tleap {}< merge", (from_block + b'a') as char);
+                                }
+                                _ => {
+                                    ltape.push(Item::Exp { block: to_block, exp: item_exp, visited: true });
+                                    ui_dbg!("\tleap {}<", (from_block + b'a') as char);
+                                }
+                            }
+                            return true;
+                        }
                     }
-                    if let Some(Item::Exp { block: 0, exp, visited }) = self.tape[0].last_mut() {
-                        *exp = exp.checked_add(item_exp).unwrap();
-                        *visited = true;
-                        ui_dbg!("\tleap a merge");
-                        return true;
-                    }
-                    self.tape[0].push(Item::Exp { block: 0, exp: item_exp, visited: true });
-                    ui_dbg!("\tleap a");
-                    return true;
                 }
             }
         }
 
-        // (011)^a <C 10 (110)^b -> <C 10 (110)^(a+b)
-        if self.head.state == 2 && self.head.direction == Direction::Left {
-            if let Some(&Item::Exp { block: 0, exp: item_exp, visited: _ }) = self.tape[0].last() {
-                // TODO: special case for <C 1 _ should speed it up
-                // or even better - accelerate whole left bouncer part that looks like 01 ^ >  & < 10 ^
-                let [ltape, rtape] = &mut self.tape;
-                if let [.., Item::Exp { block: 0, exp, visited }, Item::S(0), Item::S(1)] = rtape.as_mut_slice() {
-                    ltape.pop();
-                    *exp = exp.checked_add(item_exp).unwrap();
-                    *visited = true;
-                    ui_dbg!("\tleap c");
-                    return true;
+        // (a|c)^n <C 10 -> <C 10 (a|d)^n
+        if self.head.state == 2 && self.head.direction == Direction::Left && rtape.ends_with(&[Item::S(0), Item::S(1)])
+        {
+            if let Some(Item::Exp { block, exp: item_exp, visited: _ }) = ltape.last().copied() {
+                for (from_block, to_block) in [(0, 0), (2, 3)] {
+                    if block == from_block {
+                        ltape.pop();
+                        rtape.truncate(rtape.len() - 2);
+                        match rtape.last_mut() {
+                            Some(Item::Exp { block, exp, visited }) if *block == to_block => {
+                                *exp = exp.checked_add(item_exp).unwrap();
+                                *visited = true;
+                                ui_dbg!("\tleap {}< merge", (from_block + b'a') as char);
+                            }
+                            _ => {
+                                rtape.push(Item::Exp { block: to_block, exp: item_exp, visited: true });
+                                ui_dbg!("\tleap {}<", (from_block + b'a') as char);
+                            }
+                        }
+                        Self::compress(rtape, blocks); // maybe could be removed - usually it goes back through `01` & then it compresses
+                        rtape.push(Item::S(0));
+                        rtape.push(Item::S(1));
+                        return true;
+                    }
                 }
             }
         }
@@ -139,17 +211,16 @@ impl Configuration {
         false
     }
 
-    /// runs until head leaves the tape, split & return new conf, C (`<S ABC` -> `<S AB` & `C`) & end conf string if in tui mode
-    fn run(mut self, machine: &Machine, blocks: RefBlocks, cfg: Config) -> Result<(), Err> {
+    fn run(&mut self, machine: &Machine, blocks: RefBlocks, cfg: Config) -> Result<(), Err> {
         while self.sim_step < cfg.sim_step_limit {
-            if !self.leap() {
+            if !self.leap(blocks) {
                 let symbol = Self::pop_symbol(&mut self.tape[self.head.direction.idx()], blocks)?;
                 let trans = machine.get_transition(symbol, self.head.state).ok_or(Err::Halt)?;
                 self.head = trans.head;
                 Self::push_symbol(trans.symbol, &mut self.tape[self.head.direction.opp_idx()], blocks);
             }
             self.sim_step += 1;
-            if self.sim_step % cfg.print_mod == 0 {
+            if self.sim_step & ((1 << cfg.print_mod) - 1) == 0 {
                 println!("{}", self);
             }
         }
@@ -240,83 +311,113 @@ impl FromStr for Configuration {
 #[derive(FromArgs, Debug)]
 /// Let's simulate
 struct Args {
-    /// simulation step limit, 10^n
+    /// simulation step limit, 2^n
     #[argh(positional)]
     sim_step_limit: u32,
-    /// how often to print configuration, 10^n
+    /// how often to print configuration, 2^n
     #[argh(positional)]
-    print_mod: u32,
+    print_mod: u8,
     /// starting configuration
     #[argh(option, default = "Configuration::new()")]
     conf: Configuration,
-    // #[argh(subcommand)]
-    // cmd: SubCommand,
+    /// tui mode
+    #[argh(switch, short = 't')]
+    tui: bool,
 }
-
-// #[derive(FromArgs, Debug)]
-// #[argh(subcommand)]
-// enum SubCommand {
-//     Run,
-//     Explore(Explore),
-// }
-
-// #[derive(FromArgs, Debug)]
-// /// Closed Position Set
-// #[argh(subcommand, name = "explore")]
-// struct Explore {
-//     /// initial configuration
-//     #[argh(option, default = "bbc::skelet_cps::SegmentSizes([0,0,0])")]
-//     pub segment_sizes: bbc::skelet_cps::SegmentSizes,
-//     /// max segment size
-//     #[argh(option, default = "20")]
-//     pub max_segment_size: usize,
-// }
 
 #[derive(Clone, Copy, Debug)]
 struct Config {
     sim_step_limit: usize,
-    print_mod: usize,
+    print_mod: u8,
 }
 
+// new run:               cargo run --release --bin no1 60 30
+// explore configuration: cargo run --release --bin no1 8 0 --conf "!  A> a^4 1010 a^2 11"
+// explore in tui:        cargo run --release --bin no1 8 0 --conf "a^61652 10 E>  d^11104" --tui
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let args: Args = argh::from_env();
-
     let machine = Machine::from("1RB1RD_1LC0RC_1RA1LD_0RE0LB_---1RC");
-
-    // let cfg = Config { sim_step_limit: 10usize.checked_pow(14).unwrap(), ui_mod: 10usize.pow(9) };
-    // let cfg = Config { sim_step_limit: 200, print_mod: 1 };
-    // let cfg = Config { sim_step_limit: 3195500000, ui_mod: 500000 };
-    let cfg = Config {
-        sim_step_limit: 10usize.checked_pow(args.sim_step_limit).unwrap(),
-        print_mod: 10usize.checked_pow(args.print_mod).unwrap(),
-    };
-
+    let args: Args = argh::from_env();
+    let cfg = Config { sim_step_limit: 2usize.checked_pow(args.sim_step_limit).unwrap(), print_mod: args.print_mod };
+    let mut conf = args.conf;
     // dbg!(cfg);
-    println!("{}", args.conf);
+    println!("{}", conf);
 
     let block_a = parse("011")?;
     let block_b = parse("11 a^15282 01 a^20689")?;
     let block_c = parse("a^144285 01 a^6153 01 a^3077 01 a^601 01 a^61653 01")?;
     let mut block_d = parse("a^144285 10 a^6153 10 a^3077 10 a^601 10 a^61653 10")?;
     block_d.reverse();
+    let block_e = parse("a^61651 001 a^1 01 a^144283 001 a^1 01 a^6151 001 a^1 01 a^3075 001 a^1 01 a^599 001 a^1 01")?;
 
-    let blocks = vec![block_a.as_slice(), block_b.as_slice(), block_c.as_slice(), block_d.as_slice()];
+    let blocks =
+        vec![block_a.as_slice(), block_b.as_slice(), block_c.as_slice(), block_d.as_slice(), block_e.as_slice()];
     // dbg!(&blocks);
 
-    let ret = args.conf.run(&machine, &blocks, cfg);
-    dbg!(&ret);
+    if args.tui {
+        tui(conf, &machine, &blocks, cfg)?;
+    } else {
+        let ret = conf.run(&machine, &blocks, cfg);
+        dbg!(&ret);
+    }
 
     Ok(())
-    // TODO: splitnut to ma medzerach & nahradit ^ za mod & printnut len ak % 2 != 0, skompresovat do unikatnych blokov
-    // spravit regulerne parse aby som sa mohol  mrknut ako to vyzera od nejakeho bodu ala akcelerovanie tej pravej strany
-    // ale najprv to porovnat s mateonovym kompress vystupom
+}
+
+fn tui(mut conf: Configuration, machine: &Machine, blocks: RefBlocks, mut cfg: Config) -> Result<()> {
+    let stdin = io::stdin();
+    let mut screen = io::stdout().into_raw_mode()?.into_alternate_screen()?;
+    write!(screen, "{}", termion::cursor::Hide).unwrap();
+
+    let mut speed = cfg.print_mod;
+    cfg.print_mod = u8::MAX; // do not print inside conf::run
+
+    let mut keys = stdin.keys();
+    let mut state: Result<(), Err> = Ok(());
+    let mut history: VecDeque<(u8, Result<(), Err>, Configuration)> = VecDeque::new();
+    loop {
+        write!(
+            screen,
+            "{}{}q: quit, j: next step, k: previous, h: slow down, l: speed up; current step speed: 2^{} == {}\r\n",
+            termion::clear::All,
+            termion::cursor::Goto(1, 1),
+            speed,
+            (1 << speed).bright_white()
+        )?;
+        write!(screen, "history size: {}, speed stack ('a' + speed):\r\n\r\n", history.len().bright_white(),)?;
+        history.iter().take(100).rev().try_for_each(|(speed, _, _)| write!(screen, "{}", (speed + b'a') as char))?;
+        write!(screen, "\r\n\r\nstate: {:?}\r\n\r\n{}", state.bright_white(), conf)?;
+        screen.flush()?;
+
+        match keys.next().unwrap().unwrap() {
+            Key::Char('q') => break,
+            Key::Char('j') => {
+                let step = 1 << speed;
+                cfg.sim_step_limit = conf.sim_step + step;
+                state = conf.run(machine, blocks, cfg);
+                if history.len() > 1_000_000 {
+                    history.pop_back();
+                }
+                history.push_front((speed, state, conf.clone()));
+            }
+            Key::Char('k') => {
+                if let Some((_, s, c)) = history.pop_front() {
+                    state = s;
+                    conf = c;
+                }
+            }
+            Key::Char('h') => speed = speed.saturating_sub(1),
+            Key::Char('l') => speed = (speed + 1).min(30),
+            _ => {}
+        }
+    }
+    write!(screen, "{}", termion::cursor::Show).unwrap();
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
@@ -344,20 +445,3 @@ mod tests {
         Ok(())
     }
 }
-
-// impl<'a> bbc::ui::Display1<RefBlocks<'a>> for Configuration {
-//     fn fmt(&self, blocks: RefBlocks, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         fn fmt_symbol(item: &Item, blocks: RefBlocks, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//             match item {
-//                 Item::S(s) => write!(f, "{s}"),
-//                 Item::Exp { block, exp } => write!(f, " {}^{} ", (block + b'a') as char, exp),
-//                 Item::Unreachable => write!(f, " | "),
-//             }
-//         }
-
-//         write!(f, "{}:  ", self.sim_step)?;
-//         self.tape[0].iter().try_for_each(|item| fmt_symbol(item, blocks, f))?;
-//         write!(f, " {} ", self.head)?;
-//         self.tape[1].iter().rev().try_for_each(|item| fmt_symbol(item, blocks, f))
-//     }
-// }
